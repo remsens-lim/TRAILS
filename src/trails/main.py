ccae4927668846ff64bc9de64abe59da6419d06d
Johanna Roschke <jroschke@kusi.int.meteo.uni-leipzig.de>
2026-08-31 11:11:05 +0200

    changed date input to take from config file in main.py

--- src/trails/main.py
+++ src/trails/main.py
@@ -26,195 +26,6 @@ import functions as funct
 # Set warnings filter
 warnings.filterwarnings("ignore")
 
-def read_flexpart_traj_meta(fname, ncluster = 5):
-    """ """
-    
-    data = {}
-    with open(fname) as f:
-        l = f.readline().split()
-        data['end_of_sim'] = l[0] + "_" + l[1].zfill(6)
-        data['version'] = l[2]
-        l = f.readline().split()
-        #print("second line? ", l)
-        l = f.readline().split()
-
-        data['no_releases'] = int(l[0])
-        data['ncluster'] = ncluster
-        data['releases_meta'] = {} 
-        data['releases_traj'] = defaultdict(lambda: defaultdict(list))
-        for i in range(1, data['no_releases']+1):
-            l = f.readline().split()
-            data['releases_meta'][i] = {}
-            data['releases_meta'][i]['start_times'] = list(map(float, l[0:2]))
-            data['releases_meta'][i]['lat_lon_bounds'] = list(map(float, l[2:6]))
-            data['releases_meta'][i]['heights'] = list(map(float, l[6:8]))
-            data['releases_meta'][i]['species'] = float(l[8])
-            data['releases_meta'][i]['no_particles'] = float(l[9])
-            data['releases_meta'][i]['string'] =  f.readline().strip()
-            #print('releases meta', data['releases_meta'][i])
-            
-        for line in f:
-            l = line.split()
-            i = int(l.pop(0))
-            
-            props = ['age', 'lon', 'lat', 'height', 'mean_topo',
-                     'mean_mlh', 'mean_tph', 'mean_PV', 'rms_distance',
-                     'rms', 'zrms_distance', 'zrms', 'frac_ml', 'frac_lt_2pvu',
-                     'frac_tp']
-            for p in props:
-                match = re.match('([-+]?[0-9]*\.?[0-9]*)(-[0-9]*\.?[0-9]*)', l[0])
-                if match and not match.group(1) == '':
-                    l[0] = match.group(1)
-                    l.insert(1, match.group(2))
-                elem = float(l.pop(0))
-                #print(p, elem)
-                data['releases_traj'][i][p].append(elem)
-                
-                
-            # cluster are not continuous
-            # fix ourselfs
-            avail_clusters = list(range(ncluster))
-            cluster_data = []
-            for k in avail_clusters:
-                cluster_props = ['lon', 'lat', 'height', 'frac', 'rms']
-                cluster_data.append({})
-                for cp in cluster_props:
-                    match = re.match('([-+]?[0-9]*\.?[0-9]*)(-[0-9]*\.?[0-9]*)', l[0])
-                    if match and not match.group(1) == '':
-                        l[0] = match.group(1)
-                        l.insert(1, match.group(2))
-                    elem = float(l.pop(0))
-                    #key = 'c{}_{}'.format(k, cp)
-                    #print(key, cp, elem)
-                    cluster_data[k][cp] = elem
-            
-
-                for ci, elem in enumerate(cluster_data):
-                    for k, v in elem.items():
-                            key = 'c{}_{}'.format(ci, k)
-                            data['releases_traj'][i][key].append(v)
-                      
-                    
-            assert len(l) == 0, "line not fully consumend"
-        
-        return data
-
-def get_quantized_ctable(dtype, cparams, quantize=None, expectedlen=None):
-    """Return a ctable with the quantize filter enabled for floating point cols.
-    
-    License
-        This function is taken from the reflexible package (https://github.com/spectraphilic/reflexible/tree/master/reflexible).
-        Authored by John F Burkhart <jfburkhart@gmail.com> with contributions Francesc Alted <falted@gmail.com>.
-        Licensed under: 'This script follows creative commons usage.'
-    """
-    columns, names = [], []
-    for fname, ftype in dtype.descr:
-        names.append(fname)
-        if 'f' in ftype:
-            cparams2 = bcolz.cparams(clevel=cparams.clevel, cname=cparams.cname, quantize=quantize)
-            columns.append(bcolz.zeros(0, dtype=ftype, cparams=cparams2, expectedlen=expectedlen))
-        else:
-            columns.append(bcolz.zeros(0, dtype=ftype, cparams=cparams, expectedlen=expectedlen))
-    return bcolz.ctable(columns=columns, names=names)
-
-def read_partpositions(filename, nspec, ctable=True, clevel=5, cname="lz4", quantize=None):
-    """Read the particle positions in `filename`.
-
-    This function strives to use as less memory as possible; for this, a
-    bcolz ctable container is used for holding the data.  Besides to be compressed
-    in-memory, its chunked nature makes a natural fit for data that needs to
-    be appended because it does not need expensive memory resize operations.
-
-    NOTE: This code reads directly from un UNFORMATTED SEQUENTIAL data Fortran
-    file so care has been taken to skip the record length at the beginning and
-    the end of every record.  See:
-    http://stackoverflow.com/questions/8751185/fortran-unformatted-file-format
-
-    Parameters
-    ----------
-    filename : string
-        The file name of the particle raw data
-    nspec : int
-        number of species in particle raw data
-    ctable : bool
-        Return a bcolz ctable container.  If not, a numpy structured array is returned instead.
-    clevel : int
-        Compression level for the ctable container
-    cname : string
-        Codec name for the ctable container.  Can be 'blosclz', 'lz4', 'zlib' or 'zstd'.
-    quantize : int
-        Quantize data to improve (lossy) compression.  Data is quantized using
-        np.around(scale*data)/scale, where scale is 2**bits, and bits is
-        determined from the quantize value.  For example, if quantize=1, bits
-        will be 4.  0 means that the quantization is disabled.
-
-    Returns
-    -------
-    ctable object OR structured_numpy_array
-
-    Returning a ctable is preferred because it is used internally so it does not require to be
-    converted to other formats, so it is faster and uses less memory.
-
-    Note: Passing a `quantize` param > 0 can increase the compression ratio of the ctable
-    container, but it may also slow down the reading speed significantly.
-
-    License
-        This function is taken from the reflexible package (https://github.com/spectraphilic/reflexible/tree/master/reflexible).
-        Authored by John F Burkhart <jfburkhart@gmail.com> with contributions Francesc Alted <falted@gmail.com>.
-        Licensed under: 'This script follows creative commons usage.'
-
-
-    """
-
-    CHUNKSIZE = 10 * 1000
-    xmass_dtype = [('xmass_%d' % (i + 1), 'f4') for i in range(nspec)]
-    # note age is calculated from itramem by adding itimein
-    out_fields = [
-                     ('npoint', 'i4'), ('xtra1', 'f4'), ('ytra1', 'f4'), ('ztra1', 'f4'),
-                     ('itramem', 'i4'), ('topo', 'f4'), ('pvi', 'f4'), ('qvi', 'f4'),
-                     ('rhoi', 'f4'), ('hmixi', 'f4'), ('tri', 'f4'), ('tti', 'f4')] + xmass_dtype
-    raw_fields = [('begin_recsize', 'i4')] + out_fields + [('end_recsize', 'i4')]
-    raw_rectype = np.dtype(raw_fields)
-    recsize = raw_rectype.itemsize
-
-    cparams = bcolz.cparams(clevel=clevel, cname=cname)
-    if quantize is not None and quantize > 0:
-        out = get_quantized_ctable(raw_rectype, cparams=cparams, quantize=quantize, expectedlen=int(1e6))
-    else:
-        out = bcolz.zeros(0, dtype=raw_rectype, cparams=cparams, expectedlen=int(1e6))
-
-    with open(filename, "rb", buffering=1) as f:
-        # The timein value is at the beginning of the file
-        reclen = np.ndarray(shape=(1,), buffer=f.read(4), dtype="i4")[0]
-        assert reclen == 4
-        itimein = np.ndarray(shape=(1,), buffer=f.read(4), dtype="i4")
-        reclen = np.ndarray(shape=(1,), buffer=f.read(4), dtype="i4")[0]
-        assert reclen == 4
-        nrec = 0
-        while True:
-            # Try to read a complete chunk
-            data = f.read(CHUNKSIZE * recsize)
-            read_records = int(len(data) / recsize)  # the actual number of records read
-            chunk = np.ndarray(shape=(read_records,), buffer=data, dtype=raw_rectype)
-            # Add the chunk to the out array
-            out.append(chunk[:read_records])
-            nrec += read_records
-            if read_records < CHUNKSIZE:
-                # We reached the end of the file
-                break
-
-    # Truncate at the max length (last row is always a sentinel, so remove it)
-    out.trim(1)
-    # Remove the first and last columns
-    out.delcol("begin_recsize")
-    out.delcol("end_recsize")
-
-    if ctable:
-        return out
-    else:
-        return out[:]
-
-
 # Configuration
 # CORRECT PATH: Go up 2 levels to project root, then into config folder
 script_dir = Path(__file__).parent  # This is /.../TRAILS/src/trails/
@@ -315,7 +126,7 @@ def process_arrival_time(args):
     folder = config['partposit_dir'] + dt.strftime("%Y%m%d_%H") + "/"
     
     # Load trajectory metadata
-    traj_meta = read_flexpart_traj_meta(folder + "trajectories.txt")
+    traj_meta = funct.read_flexpart_traj_meta(folder + "trajectories.txt")
     
     # Precompute particle times
     part_times = [dt - datetime.timedelta(hours=3*i) for i in reversed(range(N_DATA))]
@@ -390,7 +201,7 @@ def process_arrival_time(args):
         frp_map = frp_cache[date_key]
 
         # Assuming part_pos is a bcolz ctable returned from read_partpositions
-        part_pos = read_partpositions(folder + f, 1, ctable=True)
+        part_pos = funct.read_partpositions(folder + f, 1, ctable=True)
         
         # Convert to a 2D numeric array: (n_records, N_DATA)
         fields = part_pos.dtype.names           # all column names
@@ -716,8 +527,12 @@ def main():
         config = toml.load(f)
     
     # Date range setup - now for multiple days
-    start_date = datetime.datetime(2023, 5, 14)
-    end_date   = datetime.datetime(2023, 5, 15)  # Example: 7 days
+    #start_date = datetime.datetime(2025, 7, 4)
+    #end_date   = datetime.datetime(2025, 7, 5)  # Example: 7 days
+    
+    start_date = datetime.datetime.strptime(config['time']['begin'], '%Y-%m-%d')
+    end_date = datetime.datetime.strptime(config['time']['end'], '%Y-%m-%d')
+    
     current_date = start_date
     # We need the memory efect also for tropospheric smoke logic, otherwise we 
     # have tropospheric smoke identification over the atlantic and too low altitudes.. which eventually increase SOF within the PBL
